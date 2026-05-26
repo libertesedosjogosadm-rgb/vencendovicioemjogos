a98df213a948fb73f35e20eccd054a2dca0a5350
libertesedosjogosadm-rgb <libertesedosjogosadm@gmail.com>
2026-05-26 20:47:17 -0300

    Add files via upload

---
+++ wa_res/generated/global.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+wf_init_animation('aos',{once: true});
+});
+WaContext.m_web_messages_developper = {
+'empty sub-folder':'Pasta vazia',
+'warning path public folder %1 %2':'Atenção: seu site pode não ser exibido corretamente.\nO local publicado não corresponde à pasta configurada no WebAcappella.\nPasta atual detectada: %1\nPasta configurada no WebAcappella: %2\nAltere sua subpasta pública na configuração de publicação e use: %1, depois publique novamente seu site.\nSe você for um usuário avançado e utilizar a reescrita de URL (arquivo .htaccess), esta mensagem pode ser ignorada.\nVocê pode desativar esta verificação nas configurações de publicação do seu site.'
+};
